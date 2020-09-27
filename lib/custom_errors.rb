2d739cdd3e9f9d0f9c7a2608a2e602fa51d24944
Alex Darvin <amdarvin14@gmail.com>
2020-09-27 18:18:03 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -11,7 +11,11 @@ class Person
   def get_married(person)
     self.partner = person
     if person.class != person
-      raise PartnerError
+      begin
+        raise PartnerError
+      rescue PartnerError => PartnerError
+        puts error.message
+      end
     else  person.partner = self
   end
 end
